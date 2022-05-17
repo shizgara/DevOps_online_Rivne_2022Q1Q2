36162aab65561184272ac227b33432bb9df7b6ad
shizgara <shizgara85@gmail.com>
2022-05-17 11:12:00 +0300

    some changes

--- m6/task 6.1/6_1.sh
+++ m6/task 6.1/6_1.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-if [ "$1" = "--all" ]
-	then
-	echo "There are the next hosts in our subnet";
+
+function all {
+	echo "There are the next hosts in our subnet"
 	bash=$(arp -n)
 	echo "$bash" | awk '{print $1 "\t" $3}'
-elif [ "$1" = "--target" ]
-	then
-	echo "There are next open TCP ports";
-	netstat=$(netstat -lt);
-	echo "$netstat" ;
-elif [[ -z $1 ]]
-	then
+}
+
+function target {
+	echo "There are next open TCP ports"
+	netstat=$(netstat -lt)
+	echo "$netstat"
+}
+
+function null {
 	echo "Choose one of the next keys"
 	echo "\"--all\" key displays the IP addresses and symbolic names of all hosts in the current subnet"
 	echo "\"--target\" key displays a list of open system TCP ports"
+}
+
+
+
+
+if [ "$1" = "--all" ]
+	then
+	all
+elif [ "$1" = "--target" ]
+	then
+	target
+elif [[ -z $1 ]]
+	then
+	null
 else
 	echo "Incorect key"
 fi
